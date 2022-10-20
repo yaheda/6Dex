586a191827402a2c7ede96599f1a1532628ddbbf
yaheda <yaheda.a@gmail.com>
2022-10-20 22:44:46 +0200

    add fake/mock erc20 tokens

--- Rep.sol
+++ Rep.sol
@@ -3,6 +3,6 @@ pragma solidity 0.6.3;
 //import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
-constract Dai is ERC20 {
-  constructor() ERC20("DAI", "Dai Stablecoin") public { }
+constract Rep is ERC20 {
+  constructor() ERC20("REP", "Augur Token") public { }
 }
