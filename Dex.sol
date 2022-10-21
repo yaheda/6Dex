6afaebde14b65721b54fe010973c13f986616603
yaheda <yaheda.a@gmail.com>
2022-10-21 18:49:27 +0200

    limit order

--- Dex.sol
+++ Dex.sol
@@ -10,11 +10,30 @@ contract Dex {
     address tokenAddress;
   }
 
+  enum Side {
+    BUY,
+    SELL
+  }
+
+  struct Order {
+    uint id;
+    Side side;
+    bytes32 ticker;
+    uint amount;
+    uint filled;
+    uint price;
+    uint date;
+  }
+
   mapping(bytes32 => Token) public tokens;
   bytes32[] public tokenList;
   address public admin;
 
   mapping(address => mapping(bytes => uint)) public traderBalances;
+
+  mapping(bytes32 => mapping(uint => Order[])) public orderBook;
+  uint public nextOrderId;
+  bytes32 constant DAI = bytes("DAI");
 
   constructor() public {
     admin = msg.sender;
@@ -38,6 +57,43 @@ contract Dex {
       .transfer(msg.sender, _amount);
 
     traderBalances[msg.sender][_ticker] -= _amount;
+  }
+
+  function createLimitOrder(bytes32 ticker, uint amount, uint price, Side side) tokenExist(_ticker) external {
+    require(ticker != DAI, "cannot trade DAI");
+    if (side == Side.SELL) {
+      require(traderBalances[msg.sender][ticker] >= amount, "token balance too low");
+    } else {
+      require(traderBalances[msg.sender][DAI] >= amount * price, "dai balance too low");
+    }
+
+    Order[] storage orders = orderBook[ticker][uint(side)];
+    orders.push(
+      nextOrderId,
+      side,
+      ticker,
+      amount,
+      0,
+      price,
+      now
+    );
+
+    //[30, 25, 22, 26] - bubble sort - buy
+    uint i = orders.length - 1;
+    while (i > 0) {
+      if (side == Side.BUY && orders[i - 1].price > orders[i].price) {
+        break;
+      }
+      if (side == Side.SELL && orders[i - 1].price < orders[i].price) {
+        break;
+      }
+      Order memory order = orders[i - 1];
+      orders[i - 1] = orders[i];
+      i--;
+    }
+
+    nextOrderId;
+
   }
 
   modifier tokenExist(bytes32 _ticker) {
